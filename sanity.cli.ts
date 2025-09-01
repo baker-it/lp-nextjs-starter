354ec32de8d63b1c8d32966f81bf6b6ec74680e5
bkr <skrytyjumper@gmail.com>
2025-09-01 21:52:27 +0200

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,5 +1,7 @@
 // sanity.cli.ts
 import {defineCliConfig} from 'sanity/cli'
+
 export default defineCliConfig({
-  api: { projectId: 'nfon9ew1', dataset: 'production' }
+  api: { projectId: 'nfon9ew1', dataset: 'production' },
+  studioHost: 'airtouch-lp-studio', // ← przypięty host, żeby CI nie pytało
 })
